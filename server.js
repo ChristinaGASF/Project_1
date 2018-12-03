41b8f5f6e2aedc18d3750dbb760a2b207c8f3aa8
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-12-03 14:14:48 -0800

    signing with google

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 const 
     express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    db = require('./models'),
-    ctrl = require('./controllers');
+    app = express();
+    // bodyParser = require('body-parser'),
+    // db = require('./models');
+    // ctrl = require('./controllers');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
 
 // serve static files from public folder
@@ -17,3 +17,5 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
   });
+
+app.listen(3000);
