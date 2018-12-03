9196a38d5f54c073cb90905a0def5201e21003d8
krdowns <krjdowns16@gmail.com>
2018-12-03 12:22:54 -0800

    added requirements in server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// require modules
+
+const 
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    db = require('./models'),
+    ctrl = require('./controllers');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json());
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+// HTML ENDPOINTS //
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+  });
