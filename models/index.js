1fee46e8cd22d3dde8bafa4d470eea6763e846e3
krdowns <krjdowns16@gmail.com>
2018-12-07 08:52:54 -0800

    removed user.js required from index.js model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,5 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/associations', { useNewUrlParser: true } );
 
 module.exports = {
-    User : require('./user'),
     Restaurant : require('./restaurant'),
 }
