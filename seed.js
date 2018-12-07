0b513075b3ffbd187149b4b3006ceb9a32a71428
ChristinaGASF <44411722+ChristinaGASF@users.noreply.github.com>
2018-12-07 07:28:20 -0800

    edited out schema format for seeded data

--- seed.js
+++ seed.js
@@ -1,17 +1,10 @@
-// This file allows us to seed our application with data
+// This file allows us to initially seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
 var db = require('./models');
 
 var restaurant_list = [
 
-// {
-//     name: "input data here - expects String",
-//     type: "input data here - expects String",
-//     rating: "input data here - expects Number",
-//     image: "input data here - expects String",
-//     id: "input data here - expects Number",
-// },
 {
     name: "Boudin Bakery",
     type: "Bakery",
